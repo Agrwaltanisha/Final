0d10066b4e7ca45ff860e3a2633e35f3ca2d684c
Agrwaltanisha <Agrwaltanisha.04@gmail.com>
2023-10-15 13:09:48 +0530

    done

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+
+<header>
+    <script>import "style.css"</script>
+  
+    <nav class="navbar">
+        <a href="style.css" class="nav-branding ">Tolkon</a>
+
+        <ul class="nav-menu">
+            <li class="nav-menu">
+                <a href="/" class="nav-link">Market</a>
+            </li>
+            <li class="nav-menu">
+                <a href="/" class="nav-link">Activity</a>
+            </li>
+                <li class="nav-menu">
+                <a href="/" class="nav-link">Features</a>
+                </li>
+            <li class="nav-menu">
+                <a href="/" class="nav-link">Connect Wallet</a>
+            </li>
+        </ul>
+        <div class="Wallet">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    </nav>
+</header>
